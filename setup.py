d60c7fa67015925514cdb7f8d0d47e06baaf04c9
alecramsay <4155251+alecramsay@users.noreply.github.com>
2023-12-13 09:19:04 -0800

    WIP

--- setup.py
+++ setup.py
@@ -1,21 +1,5 @@
 from setuptools import setup, find_packages
 
-"""
-https://python-packaging.readthedocs.io/en/latest/minimal.html
-https://packaging.python.org/en/latest/specifications/declaring-project-metadata/
-
-Once:
-
-$ python3 -m pip install --upgrade build
-$ python3 -m pip install --upgrade twine
-
-Each iteration:
-
-$ python3 -m build
-$ python3 -m twine upload --repository pypi dist/rdascore-x.y.z*.*
-
-"""
-
 setup(
     name="rdascore",
     version="2.0.5",
