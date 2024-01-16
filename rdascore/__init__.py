ec3ea892e40d3a9ee8eebe95cc64f6203e8bef93
alecramsay <4155251+alecramsay@users.noreply.github.com>
2024-01-16 07:44:13 -0800

    Tweaked exports

--- rdascore/__init__.py
+++ rdascore/__init__.py
@@ -9,8 +9,9 @@ from .load import (
 )
 from .analyze import (
     analyze_plan,
+    aggregate_data_by_district,
     aggregate_shapes_by_district,
-    calc_compactness_metrics,
+    calc_compactness_metrics,  # Why is this exported? What uses it?
 )
 
 name: str = "rdascore"
