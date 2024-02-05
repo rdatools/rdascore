e9c354282a5aee3c27f558af07b513ff19c00495
alecramsay <4155251+alecramsay@users.noreply.github.com>
2024-02-04 18:13:03 -0800

    Pushed 2.4.0 - fleshed out 'analyze' exports

--- rdascore/__init__.py
+++ rdascore/__init__.py
@@ -4,7 +4,12 @@ from .analyze import (
     analyze_plan,
     aggregate_data_by_district,
     aggregate_shapes_by_district,
-    calc_compactness_metrics,  # Why is this exported? What uses it?
+    calc_population_deviation,
+    calc_partisan_metrics,
+    calc_minority_metrics,
+    calc_compactness_metrics,
+    calc_splitting_metrics,
+    rate_dimensions,
 )
 
 name: str = "rdascore"
