f1f499e5862ef211408a4f8e30f542626720fece
alecramsay <4155251+alecramsay@users.noreply.github.com>
2024-09-16 05:55:18 -0700

    Updated to new rdabase

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="rdascore",
-    version="2.7.3",
+    version="2.7.4",
     description="Redistricting analytics for scoring ensembles of redistricting plans",
     url="https://github.com/rdatools/rdascore",
     author="alecramsay",
